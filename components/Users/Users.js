0772194081e146d0d381e011d1aca0129dc7ae08
curtdragon <tealturboser@gmail.com>
2019-05-15 21:17:28 -0500

    more work on objects

--- components/Users/Users.js
+++ components/Users/Users.js
@@ -1,7 +1,14 @@
-class Users extends Cities{
+class User extends Cities{
     constructor(user){
         super(user);
         this.name = user.name;
-        this.this.email = user.email;
+        this.email = user.email;
     }
 }
+
+const curtis = new User({
+    name: "Curtis",
+    email: "tealturboser@gmail.com",
+    city: "Mount Juliet",
+    state: "Tennessee",
+});
